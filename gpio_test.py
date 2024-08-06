e7e1c74298c36ba4e429f7dc9a3a3c56dd7e9d07
Z3r0c0k3 <hackerj1128@gmail.com>
2024-08-06 20:14:52 +0900

    gpio test v2

--- gpio_test.py
+++ gpio_test.py
@@ -8,19 +8,19 @@ GPIO_PIN = 17  # 사용할 GPIO 핀 번호를 입력하세요.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN)
 
-try:
-    while True:
+while True:
+    try:
         if GPIO.input(GPIO_PIN) == GPIO.HIGH:
             print("GPIO{} is HIGH".format(GPIO_PIN))
         else:
             print("GPIO{} is LOW".format(GPIO_PIN))
-
+    
         # 0.5초마다 상태 확인
         time.sleep(0.5)
-
-except KeyboardInterrupt:
-    print("종료합니다.")
-
-finally:
-    # GPIO 정리
-    GPIO.cleanup()
+    
+    except KeyboardInterrupt:
+        print("종료합니다.")
+    
+    finally:
+        # GPIO 정리
+        GPIO.cleanup()
